29a544f933c5f36cada3b625ea082ed0fa058009
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-05-04 20:01:09 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
