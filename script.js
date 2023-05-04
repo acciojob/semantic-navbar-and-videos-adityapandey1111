88dc690a98d2eac443106cc044dd9ef988e17225
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-05-04 19:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-//your code here
-const express = require('express');
-const path = require('path');
 
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
 //your code here
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
